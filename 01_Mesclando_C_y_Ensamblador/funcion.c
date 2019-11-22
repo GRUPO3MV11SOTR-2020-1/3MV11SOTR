38ed72b0082d86b832f7636e86db2527a92c5a89
U-UPIITA\profesor <profesor@D0182.ad.udi.upiita.ipn.mx>
2019-11-21 18:58:46 -0600

    2019.11.21.19.57 Se modifico el contenido de 01_Mezclando C y Ensamblador

---
+++ 01_Mesclando_C_y_Ensamblador/funcion.c
@@ -0,0 +1,8 @@
+void funcion()
+{ }/*deliberately empty*/
+
+char funcion_exit(char c)
+{ 
+  return c;
+}/*deliberately empty*/
+
--- 01_Mesclando_C_y_Ensamblador/main.c
+++ 01_Mesclando_C_y_Ensamblador/main.c
@@ -1,5 +1,16 @@
+void funcion();
+char funcion_exit(char);
+
+#ifndef VERSION_JULIO
 void main(void)
+#else
+int main()
+#endif
 {
   funcion();
+#ifndef VERSION_JULIO
   funcion_exit(255);
+#else
+  return funcion_exit(255);
+#endif
 }
